a1f222e78aa4a22b3c0d1bc26cd22e9b54f9f7c3
Muhammed Taha Öztürk <tahaozturk_7890@hotmail.com>
2026-02-24 15:58:05 +0300

    feat: add semantic HTML portfolio structure

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   <h2>LAB -1</h2>
   <p>Ad Soyad: [Muhammed Taha Öztürk]</p>
   <p>Ogrenci No: [230541056]</p>
-  <p>Alanlar: [Yazılım,Muhasebe,Finans]</p>
+  <p>Alanlar: [Yazılım,Muhasebe,Finans]</p>g
   <p>Üniversite: [Fırat Üniversitesi Yazılım Mühendisliği]</p>
   </div>
   )
