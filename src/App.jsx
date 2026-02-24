871498f5520715abcae494302620b25f7579e5b0
Muhammed Taha Öztürk <tahaozturk_7890@hotmail.com>
2026-02-24 13:45:20 +0300

    chore: initial project setup with Vite + React + TS

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+  <div>
+  <h1>Web Tasarimi ve Programlama </h1>
+  <h2>LAB -1</h2>
+  <p>Ad Soyad: [Muhammed Taha Öztürk]</p>
+  <p>Ogrenci No: [230541056]</p>
+  </div>
+  )
+   }
+
+   export default App
